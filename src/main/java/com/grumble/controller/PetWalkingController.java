cd2ea95f4f56dd8f3c7bb38e9b5c2331a42ffe5a
Michael Brown <Michael.E.Brown@target.com>
2020-10-06 11:16:25 -0600

    Adding in stubs for other controllers

---
+++ src/main/java/com/grumble/controller/PetWalkingController.java
@@ -0,0 +1,15 @@
+package com.grumble.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class PetWalkingController {
+
+    public PetWalkingController() {
+
+    }
+
+    //TODO: implement
+}
